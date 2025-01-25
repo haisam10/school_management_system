38bdc81d977470aa2a6b1add81db09564637d094
Haisam <122108214+haisam10@users.noreply.github.com>
2025-01-26 01:07:13 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    int id;
+    char name[50];
+    char grade[5];
+};
+
+void addStudent() {
+    
+}
